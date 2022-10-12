0b2b16da339b22e66696803b54969c14e9eae5f7
Chris George <christg101@gmail.com>
2022-10-12 15:02:21 -0500

    adds basic comments to app.py

--- Pacemaker_interface/app.py
+++ Pacemaker_interface/app.py
@@ -1,3 +1,13 @@
+'''
+This file imports many modules from the PyQt.5 environment to generate a GUI. The last few lines define an 
+Application, and a Window (with size, title, and ability to exit).
+
+The benefit of the PyQt environment is to generate well-formatted code for Graphical Interfaces given a 
+custom build in PyQt Designer, a graphical user interface designer. 
+PyQt is able to generate Python code from Qt Designer. As we do not need to distribute the GUI commercially,
+we determined this environment to suit our front-end needs.
+'''
+
 import sys
 
 from PyQt5.QtWidgets import (
@@ -17,3 +27,6 @@ if __name__ == "__main__":
     win = Window()
     win.show()
     sys.exit(app.exec())
+
+
+    
