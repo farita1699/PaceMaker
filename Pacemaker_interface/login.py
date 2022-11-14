d97c491a94a6074b861008a4021fef33a7249a33
Chris George <christg101@gmail.com>
2022-11-14 15:56:10 -0500

    This commit adds popup error functionality for an invalid username/password

--- Pacemaker_interface/login.py
+++ Pacemaker_interface/login.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 
 from frontend.main import Main
-from backend.login import check_duplicate, create_new_user, login_check, check_exceed_max_users, check_new_password
+from backend.login import check_duplicate, create_new_user, login_check, check_exceed_max_users, check_new_password, check_new_username
 
 class Ui_LoginForm(object):
     def setupUi(self, LoginForm):
@@ -109,9 +109,17 @@ class Ui_LoginForm(object):
         elif (input == 2):
             msg.setText("Duplicate username detected")
         elif(input == 3):
-            msg.setText("Inappropriate Password")
+            msg.setText("Inappropriate Password: You must have a minimum 8 character password with an uppercase and lowercase letter, no spaces, and at least 1 digit.")
+        elif(input == 4):
+            msg.setText("Inappropriate Username: You must have a username with no spaces!")
         else:
             msg.setText("Test")
+        x = msg.exec_()
+
+    def show_login_popup(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("PaceMaker Login")
+        msg.setText("Your Username and/or Password is invalid")
         x = msg.exec_()
 
     def register(self):
@@ -125,6 +133,9 @@ class Ui_LoginForm(object):
         elif(check_new_password(self.PasswordInput.text())):
             print("Inappropriate Password")
             self.show_register_popup(3)
+        elif(check_new_username(self.UsernameInput.text())):
+            print("Inappropriate Username")
+            self.show_register_popup(4)
         else:
             create_new_user(self.UsernameInput.text(), self.PasswordInput.text())   
 
@@ -133,6 +144,10 @@ class Ui_LoginForm(object):
             self.main = Main()
             self.main.show()
             print("Successful")
+        else:
+            self.show_login_popup()
+            
+            
 
          
     def retranslateUi(self, LoginForm):
