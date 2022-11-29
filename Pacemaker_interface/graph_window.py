61509a8d0c5440d4ad7afe0f37067a356f256dd4
farita1699 <uwanjerry25@gmail.com>
2022-11-28 20:28:07 -0500

    two lines on graph

--- Pacemaker_interface/graph_window.py
+++ Pacemaker_interface/graph_window.py
@@ -26,29 +26,43 @@ class GraphWindow(PlotWidget):
         self._ventricular_data = np.array([])
         self.maxvals = 0
 
-        self.plot = plot.plot(pen=(0, 229, 255))
+        self._atrial_plot = plot.plot(pen=(0, 229, 255))
+        self._ventricular_plot = plot.plot(pen=(255, 255, 255))
         self._plot()
 
 
     def _plot(self):
         time_period = 10
-        plot_wid = self.plot.getViewWidget()
-        plot_wid.disableAutoRange()
-        if self.maxvals <= 400:
+        a_plot_wid = self._atrial_plot.getViewWidget()
+        a_plot_wid.disableAutoRange()
+
+        v_plot_wid = self._ventricular_plot.getViewWidget()
+        v_plot_wid.disableAutoRange()
+        if self.maxvals <= 200:
             size = len(self._atrial_data)
             x = np.arange(0, size) / time_period
-            self.plot.setData(x, self._atrial_data)
+            a_plot_wid.plot()
+            v_plot_wid.plot()
+            self._atrial_plot.setData(x, self._atrial_data)
+            self._ventricular_plot.setData(x, self._ventricular_data)
             size = size/time_period
             if size % 5 == 0:
-                plot_wid.setXRange(size-4,size+6)
-                plot_wid.enableAutoRange(x=False, y=True)
+                a_plot_wid.setXRange(size-4,size+6)
+                a_plot_wid.enableAutoRange(x=False, y=True)
+
+                v_plot_wid.setXRange(size-4,size+6)
+                v_plot_wid.enableAutoRange(x=False, y=True)
         else:
-            x = np.arange(self.maxvals - 400, self.maxvals) / time_period
-            self.plot.setData(x, self._atrial_data)
+            x = np.arange(self.maxvals - 200, self.maxvals) / time_period
+            self._atrial_plot.setData(x, self._atrial_data)
+            self._ventricular_plot.setData(x, self._ventricular_data)
             position = self.maxvals/time_period
             if position % 5 == 0:
-                plot_wid.setXRange(position-4,position+6)
-                plot_wid.enableAutoRange(x=False, y=True)
+                a_plot_wid.setXRange(position-4,position+6)
+                a_plot_wid.enableAutoRange(x=False, y=True)
+
+                v_plot_wid.setXRange(size-4,size+6)
+                v_plot_wid.enableAutoRange(x=False, y=True)
         self.maxvals = self.maxvals + 1
         
     def update(self, atrial_voltage: float, ventricular_voltage: float):
